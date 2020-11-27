74cbb59be5b7689dbe091442f50fae007909bcf7
Jhoabe Leão <jhoabe.leao@hotmail.com>
2020-11-26 23:34:50 -0300

    Aula Babel

---
+++ public/script.js
@@ -0,0 +1,5 @@
+/**
+ * Babel: Converter(transpilar) código do React para um código que o browser entenda.
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) eu vou converter de uma maneira diferente.
+ * Loaders: babel-loader, css-loader, image-loader
+ */
